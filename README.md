ab3564995d9b7d3905cad76faed3fbd672c15dc1
Paul Woods <mr.paul.woods@gmail.com>
2024-03-09 19:47:04 -0600

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gateway-and-clients
+example showing a spring boot gateway, and 2 react clients
---
+++ client1/src/App.tsx
@@ -0,0 +1,14 @@
+export default function App() {
+
+    return (
+        <>
+            <h1>Client 1</h1>
+            <ul>
+                <li><a href="/client1">Client 1</a></li>
+                <li><a href="/client2">Client 2</a></li>
+            </ul>
+
+        </>
+    )
+}
+
